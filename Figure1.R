7ff8a7c84424f43d122e644d9c2ee388da3e39fd
Nuno Moniz <nunompmoniz@gmail.com>
2016-04-12 11:38:12 +0100

    Added files via upload

--- Figure1.R
+++ Figure1.R
@@ -9,7 +9,7 @@ y <- y[order(y,decreasing=TRUE)]
 
 data <- data.frame(y=y)
 
-p1 <- ggplot(data,aes(x=seq(1,100,by=1),y=y)) + geom_point() + geom_vline(xintercept=10) + geom_vline(xintercept=25) + geom_vline(xintercept=50) + xlab("Position in Ranking") + ylab("y") + scale_x_continuous(expand=c(0,0), breaks=c(1,25,50,75,100)) + scale_y_continuous(expand=c(0,0)) + expand_limits(y=-20) + expand_limits(y=1020) + expand_limits(x=-2) + expand_limits(x=102) + theme(plot.margin = unit(c(0.5, 0.2, 0.5, 0.3), "lines"))
+p1 <- ggplot(data,aes(x=seq(1,100,by=1),y=y)) + geom_point() + geom_vline(xintercept=10) + geom_vline(xintercept=25) + geom_vline(xintercept=50) + xlab("Position in Ranking") + ylab("y") + scale_x_continuous(expand=c(0,0), breaks=c(1,25,50,75,100)) + scale_y_continuous(expand=c(0,0)) + expand_limits(y=-20) + expand_limits(y=1020) + expand_limits(x=-2) + expand_limits(x=102) + theme(plot.margin = unit(c(0.5, 0.2, 0.5, 0.3), "lines")) + annotate("text",x=5,y=400,label="3") + annotate("text",x=17.5,y=400,label="2") + annotate("text",x=37.5,y=400,label="1") + annotate("text",x=75,y=400,label="0")
 p2 <- ggplot(data,aes(x=factor(1),y=y)) + geom_boxplot() + scale_y_continuous(expand = c(0, 0)) + expand_limits(y=c(-20,1020)) + theme(axis.text = element_blank(), axis.title = element_blank(), axis.ticks = element_blank(), plot.margin = unit(c(0.5, 1, 1.6, 0), "lines")) + xlab("Position")
 
 gt1 <- ggplot_gtable(ggplot_build(p1))
